7f48c7d8f4354a3fe60c831feb650aeeaa8b6416
JEkanem <jeremiahekanem2@gmail.com>
2024-05-19 22:34:01 +0100

    clerk webhooks

--- middleware.ts
+++ middleware.ts
@@ -1,13 +1,28 @@
 // noinspection TypeScriptValidateTypes
 
-import { clerkMiddleware } from "@clerk/nextjs/server";
+// import { clerkMiddleware } from "@clerk/nextjs/server";
+//
+// export default clerkMiddleware({
+//     publicRoutes: ["/", "/api/webhook/clerk", "/admin", "/api/webhooks/user"],
+//     ignoredRoutes: ["/api/webhook/clerk"],
+// });
+//
+//
+// export const config = {
+//     matcher: ["/((?!.+.[w]+$|_next).*)", "/", "/(api|trpc)(.*)"],
+// };
 
-export default clerkMiddleware({
-    publicRoutes: ["/", "/api/webhook/clerk", "/admin", "/api/webhooks/user"],
+
+import {authMiddleware} from "@clerk/nextjs/server";
+
+export default authMiddleware({
+    // An array of public routes that don't require authentication.
+    publicRoutes: ["/api/webhook/clerk"],
+
+    // An array of routes to be ignored by the authentication middleware.
     ignoredRoutes: ["/api/webhook/clerk"],
 });
 
-
 export const config = {
-    matcher: ["/((?!.+.[w]+$|_next).*)", "/", "/(api|trpc)(.*)"],
+    matcher: ["/((?!.*\\..*|_next).*)", "/", "/(api|trpc)(.*)"],
 };
